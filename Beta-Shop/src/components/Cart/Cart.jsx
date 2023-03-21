d24f8b149c54151f23e19d98e6a8e2b506d95bd3
AssasinCread <119432888+AssasinCread@users.noreply.github.com>
2023-03-21 20:35:24 +0400

    Add files via upload

--- Beta-Shop/src/components/Cart/Cart.jsx
+++ Beta-Shop/src/components/Cart/Cart.jsx
@@ -1,6 +1,41 @@
+import { MdClose } from "react-icons/md";
+import { BsCartX } from "react-icons/bs";
+import CartItem from "./CartItem/CartItem"
 import "./Cart.scss";
-const Cart = () => {
-    return <div>Cart</div>;
+const Cart = ({ setShowCart }) => {
+  return (
+    <div className="cart-panel">
+      <div className="opac-layer"></div>
+      <div className="cart-content">
+        <div className="cart-header">
+          <span className="heading">Shopping Cart</span>
+          <span className="close-btn" onClick={() => setShowCart(false)}>
+            <MdClose />
+            <span className="text">close</span>
+          </span>
+        </div>
+        {/* <div className="empty-cart">
+            <BsCartX />
+            <span>The is no products in the cart.</span>
+            <button className="return-to-shop">RETURN TO SHOP</button>
+        </div> */}
+
+        <>
+            <CartItem />
+            <div className="cart-footer">
+                <div className="sumtotal">
+                    <span className="text">Sumtotal:</span>
+                    <span className="text total">&#8380;500</span>
+                </div>
+                <div className="button">
+                    <button className="chekout-btn">Checkout</button>
+                </div>
+            </div>
+        </>
+
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
--- Beta-Shop/src/components/SingleProduct/SingleProduct.jsx
+++ Beta-Shop/src/components/SingleProduct/SingleProduct.jsx
@@ -1,6 +1,63 @@
+import RelatedProducts from "./RelatedProducts/RelatedProducts";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedin,
+  FaPinterest,
+  FaCartPlus,
+} from "react-icons/fa";
 import "./SingleProduct.scss";
+import prod from "../../assets/products/earbuds-prod-1.webp";
 const SingleProduct = () => {
-    return <div>Single Product</div>;
+  return (
+    <div className="single-product-main-content">
+      <div className="layout">
+        <div className="single-product-page">
+          <div className="left">
+            <img src={prod} alt="product" />
+          </div>
+          <div className="right">
+            <span className="name">Product name</span>
+            <span className="price">Price</span>
+            <span className="desc">Product description</span>
+            {/* ///////////////////////////////////////////////////// */}
+            <div className="cart-buttons">
+              <div className="quantity-buttons">
+                <span>-</span>
+                <span>0</span>
+                <span>+</span>
+              </div>
+              {/* ///////////////////////////////////////////////////// */}
+              <button className="add-to-cart-button">
+                <FaCartPlus size={"20px"} />
+                ADD TO CART
+              </button>
+            </div>
+            {/* ///////////////////////////////////////////////////// */}
+            <span className="divider" />
+            <div className="info-item">
+              <span className="text-bold">
+                Category:
+                <span>Headhpones</span>
+              </span>
+              <span className="text-bold">
+                Share:
+                <span className="social-icons">
+                  <FaFacebookF size={16} />
+                  <FaTwitter size={16} />
+                  <FaInstagram size={16} />
+                  <FaLinkedin size={16} />
+                  <FaPinterest size={16} />
+                </span>
+              </span>
+            </div>
+          </div>
+        </div>
+        <RelatedProducts />
+      </div>
+    </div>
+  );
 };
 
 export default SingleProduct;
